312dbb9566e500c5de8e1cd285e73182b093b1d7
F0rest_zj <f0rever9sone9@gmail.com>
2019-03-24 17:22:00 +0800

    190324: solve one extension: tar.gz and solve file size problems

--- file operations/get_file_info.py
+++ file operations/get_file_info.py
@@ -8,21 +8,28 @@ import time
 
 def get_file_info(filePath):
 # get file name, fullpath and other properties use os.path
+# extension like tar.gz will output tar.gz, but only deal with this one
+# extension
     files = os.listdir(filePath)
     for f in files:
         filename, ext = os.path.splitext(os.path.basename(filePath + f))
-        ext = ext.split('.')[-1]
+        if ext == '.gz':
+            ext = filename.split('.')[-1] + ext
+            filename = filename.split('.')[0]
+        else:
+            ext = ext.split('.')[-1]
+
         info = os.path.getsize(filePath + f)
         if info < 1024:
-            info = str(info) + 'B'
-        elif info >= 1024 & info < 1048576:
-            info = str(info / 1024) + 'KB'
-        elif info >= 1048576 & info < 1073741824:
-            info = str(info / 1048576) + 'MB'
+            info = str('%.2f' % info) + 'B'
+        elif info < 1048576:
+            info = str('%.2f' % (info / 1024)) + 'KB'
+        elif info < 1073741824:
+            info = str('%.2f' % (info / 1048576)) + 'MB'
         else:
-            info = str(info / 1073741824) + 'GB'
+            info = str('%.2f' % (info / 1073741824)) + 'GB'
         print('full path: ' + filePath + f + '\nfile name: ' + filename
-              + '\nextension: ' + ext +'\nfile size: ' + info)
+              + '\nextension: ' + ext + '\nfile size: ' + info)
         print('create date: %s' % time.ctime(os.path.getctime(filePath + f)))
         print('last modified date: %s' % time.ctime(os.path.getmtime(filePath
                                                                      + f)) + '\n')
